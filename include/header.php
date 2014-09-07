47190669d8e004d409541eb9c3f621de92d625db
Nirelz <jimmy.hedenskog@hotmail.com>
2014-09-07 22:42:21 +0200

    yolo

---
+++ include/header.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <meta charset="UTF-8">
+  <meta name="description" content="Web Design">
+  <meta name="author" content="Nirelz">
+  <title>Return of the Storm</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="icon" type="image/png" href="images/favicon.ico">
+</head>
+<body>
+  <div id="top">
+    <a href="">Dashboard</a>
+    <a href="">User Panel</a>
+  </div>
